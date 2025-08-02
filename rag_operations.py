2c133e0c3546c0618ea5b5d4bad1b2c2020eb2d7
DPTPhatUS <tuanphat51sp@gmail.com>
2025-08-03 02:42:41 +0700

    Update rag_operations.py

--- rag_operations.py
+++ rag_operations.py
@@ -1,10 +1,26 @@
 from chromadb.api import ClientAPI
+import ollama
+
+
+prompt = """
+You are a world knowledge expert. Given the information below, answer the question clearly and naturally. Use the context to inform your response, but do not refer to the context explicitly or repeat it unnecessarily.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer:"""
 
 
 def generate_answer_with_rag(query, chunks):
     print("Generating answer using RAG...")
+
     context = "\n".join(chunks)
-    return f"Based on your documents, here's a summary related to: {query}"
+    formatted_prompt = prompt.format(context=context, question=query)
+    response = ollama.generate(model="qwen3:4b", prompt=formatted_prompt, think=False)
+    return response.response
 
 
 def search_vector_db(db: ClientAPI, query_embedding):
@@ -12,4 +28,4 @@ def search_vector_db(db: ClientAPI, query_embedding):
 
     collection = db.get_or_create_collection(name="knowledge_rag")
     results = collection.query(query_embeddings=query_embedding)
-    return results
+    return results["documents"][0]
